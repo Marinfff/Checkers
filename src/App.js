de24b29404f0b641f145c2fcf8731f245df6919a
Marin Bumbac Victor <43542642+Marinfff@users.noreply.github.com>
2019-02-13 17:05:03 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-class Table extends Component{
+class Table extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -9,11 +9,12 @@ class Table extends Component{
     };
   }
 
-  render(){
+  render() {
     var cells = [];
-    var pox = 0, poy = 0 ;
+    var pox = 0,
+      poy = 0;
 
-    function CreateTable(posY,posX,color) {
+    function CreateTable(posY, posX, color) {
       this.posX = posX;
       this.posY = posY;
       this.color = color;
@@ -21,44 +22,55 @@ class Table extends Component{
 
     for (var i = 0; i < 8; i++) {
       for (var j = 0; j < 4; j++) {
-        if ((i % 2) === 0) {
+        if ((i % 2) == 0) {
           cells.push(
-            new CreateTable (poy, pox, 'whitecell')
-          );
-          pox += 50; 
-          cells.push(
-            new CreateTable (poy, pox , 'blackcell')
-          );
-          pox += 50; 
-        }else{
-          cells.push(
-            new CreateTable (poy, pox , 'blackcell')
-          );
-          pox += 50; 
-          cells.push(
-            new CreateTable (poy, pox , 'whitecell')
+            new CreateTable(poy, pox, 'whitecell')
           );
           pox += 50;
-        } 
+          cells.push(
+            new CreateTable(poy, pox, 'blackcell')
+          );
+          pox += 50;
+        } else {
+          cells.push(
+            new CreateTable(poy, pox, 'blackcell')
+          );
+          pox += 50;
+          cells.push(
+            new CreateTable(poy, pox, 'whitecell')
+          );
+          pox += 50;
+        }
       }
       poy += 50;
       pox = 0;
     }
 
-    var handleClick = (e)=> {
+    var handleClick = (e) => {
       var arr = [e.pageY, e.pageX];
       this.props.updateData(arr);
     }
 
-    return(
-      <div className ='table'>
-          {cells.map((value, index)=>{
-          return(
-            <div className ={value.color} style = {{ top: value.posY + 'px', left: value.posX + 'px'}} onClick={(e) => handleClick(e)}>
+    return ( 
+    <div className = 'table' > {
+        cells.map((value, index) => {
+          return ( 
+          <div className = {
+              value.color
+            }
+            style = {
+              {
+                top: value.posY + 'px',
+                left: value.posX + 'px'
+              }
+            }
+            onClick = {
+              (e) => handleClick(e)
+            } >
             </div>
-            ); 
-        })}
-      </div>
+          );
+        })
+      } </div>
     );
   }
 }
@@ -67,110 +79,165 @@ class Figure extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      figselect : null
+      figselect: null
     };
   }
 
   render() {
-    var fRowLeft = 50, fRowTop = 0,
-    black = "black", white = "white";
+    var figround, cellround;
 
-var chess = [];
-function CreateObj(posY,posX,color) {
-    this.posX = posX;
-    this.posY = posY;
-    this.color = color;
-    this.right = function(){
-        this.posY += -50;
-        this.posX += 50;
+    var handleClick = (e) => {
+      var arr = [e.pageY, e.pageX];
+      this.setState({
+        figselect: arr
+      });
     }
-    this.left = function(){
-        this.posY += -50;
-        this.posX += -50;
+
+    var figselect = this.state.figselect;
+    var cellselect = this.props.cellpos;
+    var Chess = this.props.chess;
+
+    var fround = (n) => {
+      while (n % 50 !== 0) {
+        n--;
+      }
+      return n;
     }
-}
 
-for (var i = 0; i < 32; i++) {
-    if (i <= 4 && i >= 1) {
-        chess.push(
-            new CreateObj (fRowTop, fRowLeft , white)
-        );
-        fRowLeft += 100;
-    }else if (i > 4 && i <= 8) {
-        chess.push(
-            new CreateObj (fRowTop + 50, fRowLeft - 450, white)
-        );
-        fRowLeft += 100;
-    }else if (i > 8 && i <= 12) {
-        chess.push(
-            new CreateObj (fRowTop + 100, fRowLeft - 800, white)
-        );
-        fRowLeft += 100;
-    }else if (i > 12 && i <= 16) {
-        chess.push(
-            new CreateObj (fRowTop + 250, fRowLeft - 1250 , black)
-        );
-        fRowLeft += 100;
-    }else if (i > 16 && i <= 20) {
-        chess.push(
-            new CreateObj (fRowTop + 300, fRowLeft - 1600, black)
-        );
-        fRowLeft += 100;
-    }else if (i > 20 && i <= 24) {
-        chess.push(
-            new CreateObj (fRowTop + 350, fRowLeft - 2050, black)
-        );
-        fRowLeft += 100;
+    var round = (arr) => {
+      var arrr = [fround(arr[0]), fround(arr[1])];
+      return arrr;
     }
-}
 
+    console.log(figselect, cellselect);
 
-  var handleClick =(e)=> {
-    var arr = [e.pageY, e.pageX];
-    this.setState({figselect : arr});
-  }
+    if (figselect !== null && cellselect !== null) {
+      figround = round(figselect);
+      cellround = round(cellselect);
 
+      for (let i = 0; i < Chess.length; i++) {
 
-var figselect =  this.state.figselect;
-var cellselect = this.props.cellpos;
-
-  if (figselect !== null && cellselect !== null) {
-    chess[14].left();
-  }
-
-  console.log(chess);
-  console.log(figselect, cellselect);
-  
-      
-
-    return(
-     <div>
-       {chess.map((value, index)=>{
-          return(
-            <div className ={value.color} style = {{ top: value.posY + 'px', left: value.posX + 'px'}} onClick={(e) => handleClick(e)}>
+        if (Chess[i].posY === figround[0] && Chess[i].posX === figround[1]) {
+          if (Chess[i].posX < cellround[1]) {
+            Chess[i].right();
+          } else {
+            Chess[i].left();
+          }
+        }
+      }
+    }
+    return ( 
+    <div > {
+        Chess.map((value, index) => {
+          return ( 
+          <div className = {
+              value.color
+            }
+            style = {
+              {
+                top: value.posY + 'px',
+                left: value.posX + 'px'
+              }
+            }
+            onClick = {
+              (e) => handleClick(e)
+            } >
             </div>
-            ); 
-        })}
-     </div>
+          );
+        })
+      } </div>
     );
   }
 }
 
 class App extends Component {
   state = {
-    selectValue : null,
+    selectValue: null,
+    chesss: []
   };
   updateData = (value) => {
-    this.setState({ selectValue: value})
+    this.setState({
+      selectValue: value
+    })
   };
 
-  render(){
+  componentWillMount() {
+    var fRowLeft = 50,
+      fRowTop = 0,
+      black = "black",
+      white = "white";
+
+    var chess = [];
+
+    function CreateObj(posY, posX, color) {
+      this.posX = posX;
+      this.posY = posY;
+      this.color = color;
+      this.right = function() {
+        this.posY += -50;
+        this.posX += 50;
+      }
+      this.left = function() {
+        this.posY += -50;
+        this.posX += -50;
+      }
+    }
+
+    for (var i = 0; i < 32; i++) {
+      if (i <= 4 && i >= 1) {
+        chess.push(
+          new CreateObj(fRowTop, fRowLeft, white)
+        );
+        fRowLeft += 100;
+      } else if (i > 4 && i <= 8) {
+        chess.push(
+          new CreateObj(fRowTop + 50, fRowLeft - 450, white)
+        );
+        fRowLeft += 100;
+      } else if (i > 8 && i <= 12) {
+        chess.push(
+          new CreateObj(fRowTop + 100, fRowLeft - 800, white)
+        );
+        fRowLeft += 100;
+      } else if (i > 12 && i <= 16) {
+        chess.push(
+          new CreateObj(fRowTop + 250, fRowLeft - 1250, black)
+        );
+        fRowLeft += 100;
+      } else if (i > 16 && i <= 20) {
+        chess.push(
+          new CreateObj(fRowTop + 300, fRowLeft - 1600, black)
+        );
+        fRowLeft += 100;
+      } else if (i > 20 && i <= 24) {
+        chess.push(
+          new CreateObj(fRowTop + 350, fRowLeft - 2050, black)
+        );
+        fRowLeft += 100;
+      }
+    }
+    if (this.state.chesss == 0) {
+      this.setState({
+        chesss: chess
+      });
+    }
+  }
+
+  render() {
     var cellposition = this.state.selectValue;
-    return(
-      <div>
-        <Figure cellpos = {cellposition}  />
-        <Table updateData = {this.updateData} />
-      </div>
+
+    return ( 
+    <div >
+      <Figure chess = {
+        this.state.chesss
+      }
+      cellpos = {
+        cellposition
+      }
+      /><Table updateData = {
+        this.updateData
+      }
+      /></div >
     );
   }
 }
